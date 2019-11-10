87368144393a1d488904a23c46bf06ba6f167ee3
forsigner <forsigner@gmail.com>
2019-11-10 23:08:03 +0800

    fix:(stook) fix setState bug

--- packages/stook/src/KeyStore.ts
+++ packages/stook/src/KeyStore.ts
@@ -1,32 +1,28 @@
-import { Dispatch, SetStateAction } from 'react';
-import { produce } from 'immer';
+import { Dispatch, SetStateAction } from 'react'
+import { produce } from 'immer'
 
 export class KeyStore<S = any> {
-  state: S;
-  setters: Dispatch<SetStateAction<S>>[] = [];
+  state: S
+  setters: Dispatch<SetStateAction<S>>[] = []
   constructor(value: any) {
-    this.state = value;
+    this.state = value
   }
 
   // TODO: handle any
   setState = (value: any) => {
+    const useImmer = typeof value === 'function' && typeof this.state === 'object'
 
-    // TODO: should be warning
-    const notUseImmer =
-      typeof value !== 'function' ||
-      typeof this.state !== 'object' ||
-      !!value({ ...this.state });
-
-    if (notUseImmer) {
-      this.state = value;
-      this.setters.forEach(setter => setter(this.state));
-      return;
+    if (!useImmer) {
+      this.state = value
+      this.setters.forEach(setter => setter(this.state))
+      return
     }
 
     const nextState = produce(this.state, draft => {
-      value(draft);
-    });
-    this.state = nextState;
-    this.setters.forEach(setter => setter(nextState));
-  };
+      value(draft)
+    })
+
+    this.state = nextState
+    this.setters.forEach(setter => setter(nextState))
+  }
 }
