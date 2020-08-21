f2b09fb36a8fc657b25bcb267c0f16084c2402d4
forsigner <forsigner@gmail.com>
2020-08-21 10:14:59 +0800

    chore: rm stook-form

--- packages/stook-form/src/decorators/Entity.ts
+++
@@ -1,7 +0,0 @@
-import { entityStore } from '../stores/entityStore'
-
-export function entity(name: string): ClassDecorator {
-  return target => {
-    entityStore.set(target, name)
-  }
-}
--- packages/stook-form/src/utils/isTouched.ts
+++
@@ -1,9 +0,0 @@
-/**
- * check form field is touched
- * @param touched touched Object
- * @param name form field name
- */
-export function isTouched(touched: any, name: string) {
-  if (touched[name]) return !!touched[name]
-  return false
-}
