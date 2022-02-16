82577dbf62138e81f7cf94965e263cf7a985f9c4
leen <forsigner@gmail.com>
2022-02-16 16:34:40 +0800

    feat: can return newState in mutate()

--- packages/stook/src/mutate.ts
+++ packages/stook/src/mutate.ts
@@ -11,9 +11,10 @@ export function mutate<S, K = string>(key: K | keyType, nextValue?: S) {
   const store = Storage.get(key)
 
   if (store && store.setState) {
-    store.setState(key, store.state, nextValue)
+    return store.setState(key, store.state, nextValue)
   } else {
     // init state, if no store exist
     Storage.set(key, { state: nextValue } as any)
+    return nextValue
   }
 }
